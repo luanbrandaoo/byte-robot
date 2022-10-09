2169262789da19923ef982762eef3866f9665c2e
Luan <luanbrandaodeoliveira@gmail.com>
2022-10-09 19:41:06 -0300

    search feature added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from generate_response import *
 from generate_voice import *
 from music import *
 from jokes import *
+from wikipedia_search import *
 import json
 
 def main(input):
@@ -13,16 +14,16 @@ def main(input):
     actions.append("print({})".format(speech))
     actions.append("speak({})".format(generate_voice(speech)))
   for action in response.split('execute_action')[1].replace('{','').replace('}','').split(','):
-    actions.append(actions(action,response))
+    actions.append(actions(action,input))
   return json.dumps(actions,ensure_ascii=False).encode('utf8')
 
-def actions(action,response):
+def actions(action,input):
   if 'joke(' in action:
     category = action.replace('joke(','')[:-1]
     return "speak({})".format(generate_voice(jokes(category)))
   if 'search(' in action:
-    category = action.replace('joke(','')[:-1]
-    return "speak({})".format(generate_voice(jokes(category)))
+    search = input.replace('que foi','').replace('quem','').replace('quando','').replace('como','').replace('pesquise','').replace('sobre','')
+    return "speak({})".format(generate_voice(wikipedia_search(search)))
 
 
 if __name__ == "__main__":
--- wikipedia_search.py
+++ wikipedia_search.py
@@ -3,7 +3,6 @@ wikipedia.set_lang("pt")
 
 def wikipedia_search(query):
     try:
-        query.replace('que foi','').replace('quem','').replace('quando','').replace('como','').replace('pesquise','').replace('sobre','')
         results = wikipedia.summary(query, sentences=2)
         return results
     except:
