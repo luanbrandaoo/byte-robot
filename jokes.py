29d3b5d7550778a35099b71ba745292fe040d481
Luan <luanbrandaodeoliveira@gmail.com>
2022-10-09 12:52:51 -0300

    jokes feature added

--- jokes.py
+++ jokes.py
@@ -1,14 +1,18 @@
 import json
-from random import choice
+from random import choice, randint
 
 with open('jokes.json', encoding='utf-8') as json_file:
 	jokes_dict = json.load(json_file)
 
 def jokes(category):
     if category == 'random':
-        return choice(jokes_dict['geral'])
-    else:
-        return choice(jokes_dict[category])
+        if randint(0, 10) > 7:
+            category, joke = choice(list(jokes_dict.items()))
+            print(category)
+        else:
+            category = "geral"
+    return choice(jokes_dict[category])
 
 if __name__ == "__main__":
-    print(jokes(input('digite a categoria: ').lower().strip()))
+    while 1:
+        print(jokes(input('digite a categoria: ').lower().strip()))
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+from unicodedata import category
 from generate_response import *
 from generate_voice import *
+from jokes import *
 import json
 
 def main(input):
   response = generate_response(input)
   speech = str(response.split('execute_action')[0])
   actions = []
-  actions.append("print({})".format(speech))
-  actions.append("speak({})".format(generate_voice(speech)))
-  for x in response.split('execute_action')[1].replace('{','').replace('}','').split(','):
-    actions.append(str(x))
+  if speech != '':
+    actions.append("print({})".format(speech))
+    actions.append("speak({})".format(generate_voice(speech)))
+  for action in response.split('execute_action')[1].replace('{','').replace('}','').split(','):
+    actions.append(actions(action))
   return json.dumps(actions,ensure_ascii=False).encode('utf8')
+
+def actions(action):
+  if 'joke(' in action:
+    category = action.replace('joke(','')[:-1]
+    return "speak({})".format(generate_voice(jokes(category)))
+
 
 if __name__ == "__main__":
     main(input("Digite o texto: "))
