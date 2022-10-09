30f81070a3179ea0bd4a4eef14aa6d54c0fc0ef2
Luan <luanbrandaodeoliveira@gmail.com>
2022-10-09 20:28:16 -0300

    bugs fixed

--- main.py
+++ main.py
@@ -14,20 +14,23 @@ def main(input):
     actions.append("print({})".format(speech))
     actions.append("speak({})".format(generate_voice(speech)))
   for action in response.split('execute_action')[1].replace('{','').replace('}','').split(','):
-    actions.append(actions(action,input))
+    print(action)
+    actions.append(action_processor(action,input))
   return json.dumps(actions,ensure_ascii=False).encode('utf8')
 
-def actions(action,input):
+def action_processor(action,input):
   if 'joke(' in action:
     category = action.replace('joke(','')[:-1]
     action_text = jokes(category)
     action_sound = generate_voice(action_text)
     return "print({})".format(action_text), "speak({})".format(action_sound)
-  if 'search(' in action:
+  elif 'search(' in action:
     search = input.replace('que foi','').replace('quem','').replace('quando','').replace('como','').replace('pesquise','').replace('sobre','')
     action_text = wikipedia_search(search)
     action_sound = generate_voice(action_text)
     return "print({})".format(action_text), "speak({})".format(action_sound)
+  else:
+    return action
 
 
 if __name__ == "__main__":
