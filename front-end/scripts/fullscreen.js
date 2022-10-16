595a076a59bf5d8ae57b2894182e31181a826919
Luan <luanbrandaodeoliveira@gmail.com>
2022-10-16 17:49:22 -0300

    front-end: fix bugs

---
+++ front-end/scripts/fullscreen.js
@@ -0,0 +1,14 @@
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+    updateScreen()
+}
