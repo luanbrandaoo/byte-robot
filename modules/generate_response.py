9d5b9783cc04009fc56e55428a285b442fbe3f58
Luan <luanbrandaodeoliveira@gmail.com>
2022-10-18 22:39:57 -0300

    back-end: restored chatterbot

--- modules/generate_response.py
+++ modules/generate_response.py
@@ -9,21 +9,18 @@ else:
     from modules.DialoGPT import *
     database_uri = 'sqlite:///modules/chatterbot_database.sqlite3'
 
-chatbot = ChatBot('Byte',logic_adapters=['chatterbot.logic.BestMatch','chatterbot.logic.MathematicalEvaluation','chatterbot.logic.TimeLogicAdapter'],storage_adapter='chatterbot.storage.SQLStorageAdapter',database_uri=database_uri)
+chatbot = ChatBot('Byte',logic_adapters=['chatterbot.logic.BestMatch'],storage_adapter='chatterbot.storage.SQLStorageAdapter',database_uri=database_uri)
 
 def generate_response(input):
     botreply = chatbot.get_response(input)
     if float(botreply.confidence) < 0.6:
         enInput = translateToEN(input)
-        reply = chatbot.get_response(enInput)
-        if float(reply.confidence) < 0.6:
-            reply = dialoGPT(enInput)
-            reply = translateFromEN(reply)
-            print('English translation: '+enInput)
-            print('Generated response: '+reply)
-            print('Portuguese translation: '+PtReply)
-        else:
-            print('Chatbot americano: '+reply)
+        reply = dialoGPT(enInput)
+        PtReply = translateFromEN(reply)
+        botreply = PtReply
+        print('English translation: '+enInput)
+        print('Generated response: '+reply)
+        print('Portuguese translation: '+PtReply)
     if 'execute_action' not in str(botreply):
         botreply=str(botreply)+" execute_action{emotion(neutral)}"
     return str(botreply)
