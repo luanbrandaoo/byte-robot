aae7e0661cb9106c49175fde37a3a89b4e57e54a
Luan <luanbrandaodeoliveira@gmail.com>
2022-10-20 21:15:07 -0300

    back-end: modificate "xlsx to train.py"

--- tools/xlsx to train.py
+++ tools/xlsx to train.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import json
+import unicodedata
+import re
 
 filename = 'falas byte.xlsx'
 
@@ -19,18 +21,18 @@ for category in pages:
             dict['patterns'] = []
             dict['responses'] = []
             dict['tag'] = category+str(a)
-            dict['patterns'].append(x[0])
+            dict['patterns'].append(re.sub('[^a-zA-Z0-9 \\\]', '', u"".join([c for c in unicodedata.normalize('NFKD', x[0]) if not unicodedata.combining(c)])).strip().lower())
             dict['responses'].append(x[1])
         else:
-            if x[1] == previous[1]:
-                dict['patterns'].append(x[0])
+            if x[0] == previous[0]:
+                dict['responses'].append(x[1])
             else:
                 list.append(dict)
                 dict = {}
                 dict['patterns'] = []
                 dict['responses'] = []
                 dict['tag'] = 'cumprimentos'+str(a)
-                dict['patterns'].append(x[0])
+                dict['patterns'].append(re.sub('[^a-zA-Z0-9 \\\]', '', u"".join([c for c in unicodedata.normalize('NFKD', x[0]) if not unicodedata.combining(c)])).strip().lower())
                 dict['responses'].append(x[1])
             if a == len(lines)-1:
                 list.append(dict)
