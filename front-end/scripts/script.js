9c9e273e1ef51fa93a06a3e4adc3a67995c82b79
Luan <luanbrandaodeoliveira@gmail.com>
2022-10-26 18:06:05 -0300

    front-end: update robot image

--- front-end/scripts/script.js
+++ front-end/scripts/script.js
@@ -40,10 +40,10 @@ function updateScreen() {
     var image = document.getElementById('robot-image')
     var imagepos = image.getBoundingClientRect()
     var root = document.querySelector(':root')
-    root.style.setProperty('--screen-top', (imagepos.top+image.offsetHeight/7.6)+'px')
-    root.style.setProperty('--screen-left', (imagepos.left+image.offsetWidth/2.70)+'px')
-    root.style.setProperty('--screen-height', (image.offsetHeight/4.95)+'px')
-    root.style.setProperty('--screen-width', (image.offsetWidth/3.88)+'px')
+    root.style.setProperty('--screen-top', (imagepos.top+image.offsetHeight/7.15)+'px')
+    root.style.setProperty('--screen-left', (imagepos.left+image.offsetWidth/2.75)+'px')
+    root.style.setProperty('--screen-height', (image.offsetHeight/5.04)+'px')
+    root.style.setProperty('--screen-width', (image.offsetWidth/3.62)+'px')
 }
 
 function sendMessage() {
