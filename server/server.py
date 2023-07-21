ebb2d6e2d292db1982e8cd491a41121464eea7eb
Luan <luanbrandaodeoliveira@gmail.com>
2023-07-21 18:01:00 -0300

    server.py updated to work with new directory

--- server/server.py
+++ server/server.py
@@ -1,29 +1,32 @@
 from flask import Flask, request, send_file
+import os
 from main import *
 
 app = Flask(__name__)
+
+front_end_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'front_end'))
 
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
 def index():
-  return send_file('front-end/index.html')
+    return send_file(os.path.join(front_end_path, 'index.html'))
 
 @app.route('/assets/<path:reqPath>')
 def getImageFile(reqPath):
-    return send_file('front-end/assets/'+reqPath)
+    return send_file(os.path.join(front_end_path, 'assets', reqPath))
 
 @app.route('/scripts/<path:reqPath>')
 def getScriptFile(reqPath):
-    return send_file('front-end/scripts/'+reqPath)
+    return send_file(os.path.join(front_end_path, 'scripts', reqPath))
 
 @app.route('/styles/<path:reqPath>')
 def getStyleFile(reqPath):
-    return send_file('front-end/styles/'+reqPath)
+    return send_file(os.path.join(front_end_path, 'styles', reqPath))
 
 @app.route("/response", methods=["GET"])
 def response():
-  input = request.args.get("input")
-  return main(input)
+    input = request.args.get("input")
+    return main(input)
 
-app.run(port=5000, debug=False)
+app.run(port=8080, debug=False)
